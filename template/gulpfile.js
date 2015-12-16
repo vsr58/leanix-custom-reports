f24e6dadfcb2c962ba7d8b92a71aae672de0c74e
Patrick Surrey <patrick.surrey@leanix.net>
2015-12-16 15:55:54 +0100

    Added template directory

---
+++ template/gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var serve = require('gulp-serve');
+var concat = require('gulp-concat');
+var del = require('del');
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('default', ['dist'], function() {});
+
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
+gulp.task('copy', ['clean'], function() {
+  return gulp.src(['./src/index.html'])
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('dist', ['copy'], function() {
+  return gulp.src([
+      './node_modules/jquery/dist/jquery.js',
+      './node_modules/lodash/index.js',
+      './node_modules/d3/d3.js',
+      './src/index.js'
+    ])
+    .pipe(concat('index.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch', ['dist'], function() {
+
+    browserSync({
+        notify: false,
+        logPrefix: 'BS',
+        // Run as an https by uncommenting 'https: true'
+        // Note: this uses an unsigned certificate which on first access
+        //       will present a certificate warning in the browser.
+        // https: true,
+        server: ['dist']
+    });
+
+    // Watch .html files
+    gulp.watch('src/*.*', ['dist', reload]);
+});
