625e1dd99f14188d14eeef19329aab7441721996
Andre Christ <andre.christ@leanix.net>
2015-12-17 15:53:52 +0100

    Generate without broccoli

--- build.sh
+++ build.sh
@@ -4,12 +4,15 @@ set -e
 function build_report {
   echo "building $1"
   cd $1
+  npm install
   gulp
   cd ..
+  mkdir -p dist/$1
+  cp -R $1/dist/* dist/$1
 }
+
+npm install
 
 # List of custom reports to be built
 build_report bubbles
 
-
-rm -rf dist && broccoli build 'dist'
