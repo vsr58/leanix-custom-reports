aba6958e95de69ca8564c1c5d418dfa3659dfb9b
Andre Christ <andre.christ@leanix.net>
2016-03-31 06:53:52 +0200

    Publish new version

--- develop/tables/index.js
+++ develop/tables/index.js
@@ -49644,6 +49644,10 @@ var FactSheetIndex = (function() {
             list.push(this.index[type][key]);
         }
 
+        list.sort(function(a, b) {
+            return a.displayName.localeCompare(b.displayName);
+        });
+
         return list;
     };
 
@@ -49897,6 +49901,9 @@ var ReportHierarchy = (function() {
 
                 var output = [];
                 for (var i = 0; i < list.length; i++) {
+                    if (list[i].level >= that.maxLevel)
+                        continue;
+
                     if (!that.tagFilter || list[i].tags.indexOf(that.tagFilter) != -1)  {
 
                         var hierarchy = {};
@@ -49967,6 +49974,147 @@ var ReportHierarchy = (function() {
 
     return ReportHierarchy;
 })();
+var ReportDataQuality = (function() {
+    function ReportDataQuality(reportSetup, tagFilter) {
+        this.reportSetup = reportSetup;
+        this.tagFilter = tagFilter;
+    }
+
+    ReportDataQuality.prototype.render = function() {
+        var that = this;
+
+        var factSheetPromise = $.get(this.reportSetup.apiBaseUrl + '/factsheets?relations=true&types[]=10&types[]=16&pageSize=-1')
+            .then(function (response) {
+                return response.data;
+            });
+
+        $.when(factSheetPromise)
+            .then(function (data) {
+
+                var fsIndex = new FactSheetIndex(data);
+
+                var list = fsIndex.getSortedList('services');
+
+
+                var getLookup = function(data) {
+                    var ret = {};
+                    for (var i = 0; i < data.length; i++) {
+                        ret[data[i]] = data[i];
+                    }
+
+                    return ret;
+                };
+
+                var output = [];
+                var markets = {};
+
+                var groupedByMarket = {};
+
+                function getGreenToRed(percent){
+                    var r = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
+                    var g = percent>50 ? 255 : Math.floor((percent*2)*255/100);
+                    return 'rgb('+r+','+g+',0)';
+                }
+
+                for (var i = 0; i < list.length; i++) {
+                    if (!that.tagFilter || list[i].tags.indexOf(that.tagFilter) != -1)  {
+
+
+                        // Extract market
+                        var re = /^([A-Z]{2,3})_/;
+                        var market = '';
+
+                        if ((m = re.exec(list[i].fullName)) !== null) {
+                            if (m.index === re.lastIndex) {
+                                re.lastIndex++;
+                            }
+                            // View your result using the m-variable.
+                            market = m[1];
+                        }
+
+                        if (!market)
+                            market = "n/a";
+
+                        if (market)
+                            markets[market] = market;
+
+                        var item = {
+                            name : list[i].fullName,
+                            type : 'App',
+                            id : list[i].ID,
+                            market : market,
+                            completion : Math.floor(list[i].completion * 100),
+                            count : ''
+                        };
+
+                        if (!(market in groupedByMarket))
+                            groupedByMarket[market] = [];
+
+                        groupedByMarket[market].push(item);
+                    }
+                }
+
+                for (var key in groupedByMarket) {
+
+                    var sum = 0;
+                    for (var i = 0; i < groupedByMarket[key].length; i++) {
+                        if (groupedByMarket[key][i].completion)
+                            sum += groupedByMarket[key][i].completion;
+                    }
+
+                    groupedByMarket[key].sort(function(a, b) {
+                        return a.completion - b.completion;
+                    });
+
+                    var avg = sum / groupedByMarket[key].length;
+
+                    output.push({
+                        name : key + ' (' + groupedByMarket[key].length + ' Applications)',
+                        type : 'Market',
+                        id : key,
+                        market : key,
+                        completion : Math.floor(avg)
+                    });
+
+                    for (var i = 0; i < groupedByMarket[key].length; i++) {
+                        output.push(groupedByMarket[key][i]);
+                    }
+                }
+
+
+                function link(cell, row) {
+                    if (row.type != 'Market')
+                        return '<a href="' + that.reportSetup.baseUrl + '/services/' + row.id + '" target="_blank">' + cell + '</a>';
+                    else
+                        return '<b>' + cell + '</b>';
+                }
+
+
+                function percentage(cell, row) {
+                    return  '<div class="percentage" style="background-color: ' + getGreenToRed(cell) + ';">' + cell + ' %</div>';
+                }
+
+                function trClassFormat(rowData,rIndex){
+                    return 'tr-type-' + rowData.type.toLowerCase();
+                }
+
+                ReactDOM.render(
+                    React.createElement("div", {className: "report-data-quality"}, 
+                        React.createElement(BootstrapTable, {data: output, striped: false, hover: true, search: true, condensed: true, exportCSV: true, trClassName: trClassFormat}, 
+                            React.createElement(TableHeaderColumn, {dataField: "id", isKey: true, hidden: true}, "ID"), 
+                            React.createElement(TableHeaderColumn, {dataField: "type", width: "80", filter: {type: "SelectFilter", options: {Market: 'Market'}}}, "Type"), 
+                            React.createElement(TableHeaderColumn, {dataField: "market", width: "80", dataAlign: "left", dataSort: false, filter: {type: "SelectFilter", options: markets}}, "Market"), 
+                            React.createElement(TableHeaderColumn, {dataField: "name", dataAlign: "left", dataSort: true, dataFormat: link, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Application Name"), 
+                            React.createElement(TableHeaderColumn, {dataField: "completion", dataAlign: "left", dataSort: true, dataFormat: percentage, filter: {type: "NumberFilter", defaultValue: {comparator: '<='}}}, "Completion")
+                        )
+                    ),
+                    document.getElementById("app")
+                );
+            });
+    };
+
+    return ReportDataQuality;
+})();
 var ReportApplicationLifecycle = (function() {
     function ReportApplicationLifecycle(reportSetup, tagFilter, title) {
         this.reportSetup = reportSetup;
@@ -49996,6 +50144,11 @@ var ReportApplicationLifecycle = (function() {
                     4: "Phase Out",
                     5: "End of Life"
                 };
+
+                var lifecycleArray = [];
+                for (var key in lifecycles) {
+                    lifecycleArray.push(lifecycles[key]);
+                }
 
                 function formattedDate(date) {
                     var d = new Date(date || Date.now()),
@@ -50135,9 +50288,9 @@ var ReportApplicationLifecycle = (function() {
                             React.createElement(TableHeaderColumn, {dataField: "id", isKey: true, hidden: true}, "ID"), 
                             React.createElement(TableHeaderColumn, {dataField: "market", width: "80", dataAlign: "left", dataSort: true, filter: {type: "SelectFilter", options: markets}}, "Market"), 
                             React.createElement(TableHeaderColumn, {dataField: "name", dataAlign: "left", dataSort: true, dataFormat: link, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Application Name"), 
-                            React.createElement(TableHeaderColumn, {dataField: "costCentre", width: "100", dataAlign: "left", dataSort: true, filter: {type: "SelectFilter", options: getLookup(costCentres)}}, "Cost Centre"), 
-                            React.createElement(TableHeaderColumn, {dataField: "appType", dataAlign: "left", dataSort: true, filter: {type: "SelectFilter", options: getLookup(appTypes)}}, "Application Type"), 
-                            React.createElement(TableHeaderColumn, {dataField: "lifecyclePhase", width: "100", dataAlign: "left", dataSort: true, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Phase"), 
+                            React.createElement(TableHeaderColumn, {dataField: "costCentre", width: "120", dataAlign: "left", dataSort: true, filter: {type: "SelectFilter", options: getLookup(costCentres)}}, "Cost Centre"), 
+                            React.createElement(TableHeaderColumn, {dataField: "appType", width: "100", dataAlign: "left", dataSort: true, filter: {type: "SelectFilter", options: getLookup(appTypes)}}, "App Type"), 
+                            React.createElement(TableHeaderColumn, {dataField: "lifecyclePhase", width: "100", dataAlign: "left", dataSort: true, filter: {type: "SelectFilter", options: getLookup(lifecycleArray)}}, "Phase"), 
                             React.createElement(TableHeaderColumn, {dataField: "lifecycleStart", width: "100", dataAlign: "left", dataSort: true, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Phase Start"), 
                             React.createElement(TableHeaderColumn, {dataField: "projectName", dataAlign: "left", dataSort: true, dataFormat: linkProject, filter: {type: "TextFilter", placeholder: "Please enter a value"}}, "Project Name"), 
                             React.createElement(TableHeaderColumn, {dataField: "projectEffect", width: "100", dataAlign: "left", dataSort: true, filter: {type: "SelectFilter", options: projectEffects}}, "Project Effect"), 
@@ -50169,13 +50322,19 @@ var ReportApplicationLifecycle = (function() {
             var report = new ReportHierarchy(reportSetup);
             break;
         case 'capability-definitions-cobra':
-            var report = new ReportHierarchy(reportSetup, 'COBRA', 'Domain Definitions COBRA');
+            var report = new ReportHierarchy(reportSetup, 'COBRA');
+            break;
+        case 'capability-definitions-bca':
+            var report = new ReportHierarchy(reportSetup, 'BCA');
             break;
         case 'capability-spend-cobra':
-            var report = new ReportCapabilitySpend(reportSetup, 'COBRA', 'Report spend by COBRA');
+            var report = new ReportCapabilitySpend(reportSetup, 'COBRA');
             break;
         case 'capability-spend-bca':
-            var report = new ReportCapabilitySpend(reportSetup, 'BCA', 'Report spend by BCA');
+            var report = new ReportCapabilitySpend(reportSetup, 'BCA');
+            break;
+        case 'data-quality':
+            var report = new ReportDataQuality(reportSetup);
             break;
         case 'capability-spend':
         default:
