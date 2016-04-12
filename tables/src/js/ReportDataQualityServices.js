6e7e4b1f6b380e132efe7f4b2233a1941eb8f601
Patrick Surrey <patrick.surrey@leanix.net>
2016-04-12 14:37:18 +0200

    Removed grouping functionality from data-quality-service report

--- tables/src/js/ReportDataQualityServices.js
+++ tables/src/js/ReportDataQualityServices.js
@@ -14,26 +14,10 @@ var ReportDataQualityServices = (function() {
 
         $.when(factSheetPromise)
             .then(function (data) {
-
                 var fsIndex = new FactSheetIndex(data);
-
                 var list = fsIndex.getSortedList('services');
 
-
-                var getLookup = function(data) {
-                    var ret = {};
-                    for (var i = 0; i < data.length; i++) {
-                        ret[data[i]] = data[i];
-                    }
-
-                    return ret;
-                };
-
                 var output = [];
-                var markets = {};
-
-                var groupedByMarket = {};
-
                 function getGreenToRed(percent){
                     var r = percent<50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
                     var g = percent>50 ? 255 : Math.floor((percent*2)*255/100);
@@ -43,68 +27,17 @@ var ReportDataQualityServices = (function() {
                 for (var i = 0; i < list.length; i++) {
                     if (!that.tagFilter || list[i].tags.indexOf(that.tagFilter) != -1)  {
 
-
-                        // Extract market
-                        var re = /^([A-Z]{2,3})_/;
-                        var market = '';
-
-                        if ((m = re.exec(list[i].fullName)) !== null) {
-                            if (m.index === re.lastIndex) {
-                                re.lastIndex++;
-                            }
-                            // View your result using the m-variable.
-                            market = m[1];
-                        }
-
-                        if (!market)
-                            market = "n/a";
-
-                        if (market)
-                            markets[market] = market;
-
                         var item = {
                             name : list[i].fullName,
                             type : 'App',
                             id : list[i].ID,
-                            market : market,
                             completion : Math.floor(list[i].completion * 100),
                             count : ''
                         };
 
-                        if (!(market in groupedByMarket))
-                            groupedByMarket[market] = [];
-
-                     //   groupedByMarket[market].push(item);
+                        output.push(item);
                     }
                 }
-
-                for (var key in groupedByMarket) {
-
-                    var sum = 0;
-                    for (var i = 0; i < groupedByMarket[key].length; i++) {
-                        if (groupedByMarket[key][i].completion)
-                            sum += groupedByMarket[key][i].completion;
-                    }
-
-                    groupedByMarket[key].sort(function(a, b) {
-                        return a.completion - b.completion;
-                    });
-
-                    var avg = sum / groupedByMarket[key].length;
-
-                    output.push({
-                        name : key + ' (' + groupedByMarket[key].length + ' Applications)',
-                        type : 'Market',
-                        id : key,
-                        market : key,
-                        completion : Math.floor(avg)
-                    });
-
-                    for (var i = 0; i < groupedByMarket[key].length; i++) {
-                        output.push(groupedByMarket[key][i]);
-                    }
-                }
-
 
                 function link(cell, row) {
                     if (row.type != 'Market')
@@ -112,7 +45,6 @@ var ReportDataQualityServices = (function() {
                     else
                         return '<b>' + cell + '</b>';
                 }
-
 
                 function percentage(cell, row) {
                     return  '<div class="percentage" style="background-color: ' + getGreenToRed(cell) + ';">' + cell + ' %</div>';
@@ -124,7 +56,7 @@ var ReportDataQualityServices = (function() {
 
                 ReactDOM.render(
                     <div className="report-data-quality">
-                        <BootstrapTable data={output} striped={false} hover={true} search={true} condensed={true} exportCSV={true} trClassName={trClassFormat}>
+                        <BootstrapTable data={output} striped={true} hover={true} search={true} condensed={true} exportCSV={true} trClassName={trClassFormat}>
                             <TableHeaderColumn dataField="id" isKey={true} hidden={true}>ID</TableHeaderColumn>
                             <TableHeaderColumn dataField="name" dataAlign="left" dataSort={true} dataFormat={link} filter={{type: "TextFilter", placeholder: "Please enter a value"}}>Application Name</TableHeaderColumn>
                             <TableHeaderColumn dataField="completion" dataAlign="left" dataSort={true} dataFormat={percentage} filter={{type: "NumberFilter", defaultValue: {comparator: '<='}}}>Completion</TableHeaderColumn>
